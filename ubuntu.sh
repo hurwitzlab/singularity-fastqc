870e455adf0094b58646a4b124db2afc351051bb
Scott G. Daniel <scottdaniel25@gmail.com>
2017-09-01 04:47:01 +0000

    getting somewhere

--- ubuntu.sh
+++ ubuntu.sh
@@ -10,10 +10,14 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
     sed -i 's/$/ universe/' /etc/apt/sources.list
     
     #essential stuff
-    apt -y --force-yes install git sudo man vim build-essential wget
+    apt -y --force-yes install git sudo man vim build-essential wget unzip
     
     #maybe dont need, add later if do:
     #curl autoconf libtool 
+    cd ~
     wget https://sourceforge.net/projects/solexaqa/files/latest/download?source=files
+    #put stuff in wget to rename it
+    #unzip
+    #move executable to /usr/local/bin or whatever
 
 %test
