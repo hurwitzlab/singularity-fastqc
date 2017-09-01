67a95b2b615928b4bc40afc0e27b373e901704f4
Scott G. Daniel <scottdaniel25@gmail.com>
2017-09-01 00:32:20 +0000

    start

---
+++ ubuntu.sh
@@ -0,0 +1,18 @@
+BootStrap: debootstrap
+OSVersion: trusty
+MirrorURL: http://us.archive.ubuntu.com/ubuntu/
+
+
+%runscript
+
+%post
+    echo "Hello from inside the container"
+    sed -i 's/$/ universe/' /etc/apt/sources.list
+    
+    #essential stuff
+    apt -y --force-yes install git sudo man vim build-essential 
+    
+    #maybe dont need, add later if do:
+    #curl autoconf libtool wget
+
+%test
