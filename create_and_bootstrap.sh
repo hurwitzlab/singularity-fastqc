3828d021a48ba16e387475eaf7543573bbbc0987
Scott G. Daniel <scottdaniel25@gmail.com>
2017-09-07 16:26:34 +0000

    need more space

--- create_and_bootstrap.sh
+++ create_and_bootstrap.sh
@@ -5,7 +5,7 @@ set -x
 echo "Hello! starting $(date)"
 
 sudo rm -rf fastqc.img
-singularity create -s 1024 fastqc.img
+singularity create -s 1536 fastqc.img
 sudo singularity bootstrap fastqc.img ubuntu.sh
 
 echo "Goodbye! ending $(date)"
